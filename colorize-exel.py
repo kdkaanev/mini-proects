ef2101493e529c9946c6af818b324256549dfe6f
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-13 14:11:16 +0300

    Update colorize-exel.py

--- colorize-exel.py
+++ colorize-exel.py
@@ -5,12 +5,13 @@ search_path = 'b.xlsx'
 target_path = 'sliter.xlsx'
 wb_obj = openpyxl.load_workbook(search_path) 
 sheet_obj = wb_obj.active
+wb = openpyxl.load_workbook(target_path)
+ws = wb['Sheet1'] #Name of the working sheet
 
 for i in range(407):
     cell_obj = sheet_obj.cell(row = i + 1, column = 1)
     my_set.add(str(cell_obj.value))
-wb = openpyxl.load_workbook(target_path)
-ws = wb['Sheet1'] #Name of the working sheet
+
 
 for i in range(180):
     cell_obj = ws.cell(row = i + 1, column = 1)
